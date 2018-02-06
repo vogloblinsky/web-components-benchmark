3492c18014fc43e69919bf480706e03c2fef0e1d
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-02-06 22:25:09 +0100

    Cleaning

--- tests/commands/addItem.js
+++
@@ -1,6 +0,0 @@
-exports.command = function (text, wait) {
-    return this.setValue('#new-todo', text)
-        .sendKeys('#new-todo', this.Keys.ENTER)
-        .waitForElementVisible(`todo-item[text="${text}"]`, wait)
-        .assert.cssClassNotPresent(`todo-item[text="${text}"]`, 'completed');
-};
--- tests/commands/removeItem.js
+++
@@ -1,7 +0,0 @@
-exports.command = function (text, wait) {
-    return this
-        .assert.elementPresent(`todo-item[text="${text}"] button`)
-        .click(`todo-item[text="${text}"] button`, () => console.log('eee'))
-        .waitForElementNotPresent(`todo-item[text="${text}"]`, wait)
-        .assert.elementNotPresent(`todo-item[text="${text}"]`)
-};
