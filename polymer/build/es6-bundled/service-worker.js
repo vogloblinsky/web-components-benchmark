a904df09cee240a1fb8212685f6657341f29f587
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-02-06 07:02:42 +0100

    feat(polymer): bump version

--- polymer/build/es6-bundled/service-worker.js
+++ polymer/build/es6-bundled/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","ba7a89bf8338dafe917e5af0ee1dc46c"]];
+var precacheConfig = [["index.html","433365e54ffc17f4290735021bda9d83"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
