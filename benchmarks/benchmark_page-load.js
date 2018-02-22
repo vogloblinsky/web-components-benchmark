612ecba399fd251f9030df8d0eb42b046293b8fe
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-02-22 10:55:40 +0100

    HTTP/2

--- benchmarks/benchmark_page-load.js
+++ benchmarks/benchmark_page-load.js
@@ -12,6 +12,8 @@ fs.ensureDirSync('benchmarks-results/vue');
 
 const numberOftests = 10;
 
+const LOCALHOST = 'https://localhost:3001';
+
 let browser;
 let page;
 let average = 0;
@@ -25,27 +27,26 @@ let processRawData = (filename, i) => {
     average += topDown.totalTime;
 
     console.log(`Top down tree total time ${i}: ${topDown.totalTime}`);
-}
+};
 
 (async () => {
-    
     for (let i = 0; i < numberOftests; i++) {
-        browser = await puppeteer.launch({ headless: true });
+        browser = await puppeteer.launch({ headless: true, ignoreHTTPSErrors: true });
         page = await browser.newPage();
-        
+
         filename = `benchmarks-results/native-shadow-dom/load-page_${i}.json`;
 
         await page.tracing.start({
             path: filename
         });
-        await page.goto('http://localhost:8080/native-shadow-dom/dist/index.html');
+        await page.goto(`${LOCALHOST}/native-shadow-dom/dist/index.html`);
         await page.tracing.stop();
 
         processRawData(filename, i);
 
         await browser.close();
     }
-    
+
     average = average / numberOftests;
 
     console.log(`\nAverage time for native : ${Math.ceil(average)} ms\n`);
@@ -76,7 +77,7 @@ let processRawData = (filename, i) => {
     average = 0;
 
     for (let i = 0; i < numberOftests; i++) {
-        browser = await puppeteer.launch({ headless: true });
+        browser = await puppeteer.launch({ headless: true, ignoreHTTPSErrors: true });
         page = await browser.newPage();
 
         filename = `benchmarks-results/polymer/load-page_${i}.json`;
@@ -84,7 +85,7 @@ let processRawData = (filename, i) => {
         await page.tracing.start({
             path: filename
         });
-        await page.goto('http://127.0.0.1:8080/polymer/build/es6-bundled/index.html');
+        await page.goto(`${LOCALHOST}/polymer/build/es6-bundled/index.html`);
         await page.tracing.stop();
 
         processRawData(filename, i);
@@ -99,7 +100,7 @@ let processRawData = (filename, i) => {
     average = 0;
 
     for (let i = 0; i < numberOftests; i++) {
-        browser = await puppeteer.launch({ headless: true });
+        browser = await puppeteer.launch({ headless: true, ignoreHTTPSErrors: true });
         page = await browser.newPage();
 
         filename = `benchmarks-results/polymer3/load-page_${i}.json`;
@@ -107,7 +108,7 @@ let processRawData = (filename, i) => {
         await page.tracing.start({
             path: filename
         });
-        await page.goto('http://127.0.0.1:8080/polymer3/build/es6-bundled/index.html');
+        await page.goto(`${LOCALHOST}/polymer3/build/es6-bundled/index.html`);
         await page.tracing.stop();
 
         processRawData(filename, i);
@@ -122,7 +123,7 @@ let processRawData = (filename, i) => {
     average = 0;
 
     for (let i = 0; i < numberOftests; i++) {
-        browser = await puppeteer.launch({ headless: true });
+        browser = await puppeteer.launch({ headless: true, ignoreHTTPSErrors: true });
         page = await browser.newPage();
 
         filename = `benchmarks-results/stencil/load-page_${i}.json`;
@@ -130,7 +131,7 @@ let processRawData = (filename, i) => {
         await page.tracing.start({
             path: filename
         });
-        await page.goto('http://127.0.0.1:8080/stencil/www/index.html');
+        await page.goto(`${LOCALHOST}/stencil/www/index.html`);
         await page.tracing.stop();
 
         processRawData(filename, i);
@@ -153,7 +154,7 @@ let processRawData = (filename, i) => {
         await page.tracing.start({
             path: filename
         });
-        await page.goto('http://127.0.0.1:8080/angular-elements/dist/index.html');
+        await page.goto('${LOCALHOST}/angular-elements/dist/index.html');
         await page.tracing.stop();
 
         processRawData(filename, i);
@@ -168,7 +169,7 @@ let processRawData = (filename, i) => {
     average = 0;
 
     for (let i = 0; i < numberOftests; i++) {
-        browser = await puppeteer.launch({ headless: true })
+        browser = await puppeteer.launch({ headless: true, ignoreHTTPSErrors: true });
         page = await browser.newPage();
 
         filename = `benchmarks-results/vue/load-page_${i}.json`;
@@ -176,7 +177,7 @@ let processRawData = (filename, i) => {
         await page.tracing.start({
             path: filename
         });
-        await page.goto('http://127.0.0.1:8080/vue.js/dist/index.html');
+        await page.goto(`${LOCALHOST}/vue.js/dist/index.html`);
         await page.tracing.stop();
 
         processRawData(filename, i);
@@ -187,5 +188,4 @@ let processRawData = (filename, i) => {
     average = average / numberOftests;
 
     console.log(`\nAverage time for Vue : ${Math.ceil(average)} ms\n`);
-    
 })();
