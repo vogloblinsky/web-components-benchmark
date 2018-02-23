590e055d24bdc7efd8a5cd6a2595caa893f351fb
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-02-23 14:41:10 +0100

    Update create-todos benchmark for http/2

--- benchmarks/benchmark_create-todos.js
+++ benchmarks/benchmark_create-todos.js
@@ -3,16 +3,19 @@ const puppeteer = require('puppeteer'),
     DevtoolsTimelineModel = require('devtools-timeline-model');
 
 fs.ensureDirSync('benchmarks-results/native-shadow-dom');
-//fs.ensureDirSync('benchmarks-results/native-shadow-dom_lit-html');
+fs.ensureDirSync('benchmarks-results/native-shadow-dom_lit-html');
 fs.ensureDirSync('benchmarks-results/polymer');
 fs.ensureDirSync('benchmarks-results/polymer3');
 fs.ensureDirSync('benchmarks-results/stencil');
+fs.ensureDirSync('benchmarks-results/stencil-prerendered');
 //fs.ensureDirSync('benchmarks-results/angular-elements');
 fs.ensureDirSync('benchmarks-results/vue');
 
 const numberOftests = 10,
     numberOfCreation = 50,
     selector = `document.querySelector('my-todo').shadowRoot.querySelector('todo-input').shadowRoot.querySelector('input')`;
+
+const LOCALHOST = 'https://localhost:3001';
 
 let browser;
 let page;
@@ -21,23 +24,24 @@ let filename;
 
 let processRawData = (filename, i) => {
     let events = require('fs').readFileSync(filename, 'utf8');
-    var model = new DevtoolsTimelineModel(events);
+    try {
+        var model = new DevtoolsTimelineModel(events);
+        var topDown = model.topDown();
+        average += topDown.totalTime;
+        console.log(`Top down tree total time ${i}: ${topDown.totalTime}`);
+    } catch (e) {
+        //console.log(e);
+    }
+};
 
-    var topDown = model.topDown();
-    average += topDown.totalTime;
-
-    console.log(`Top down tree total time ${i}: ${topDown.totalTime}`);
-}
-
-(async () => {   
-
+(async () => {
     for (let i = 0; i < numberOftests; i++) {
-        browser = await puppeteer.launch({ headless: true });
+        browser = await puppeteer.launch({ headless: true, ignoreHTTPSErrors: true });
         page = await browser.newPage();
-        
+
         filename = `benchmarks-results/native-shadow-dom/create-todos_${i}.json`;
 
-        await page.goto('http://localhost:8080/native-shadow-dom/dist/index.html');
+        await page.goto(`${LOCALHOST}/native-shadow-dom/dist/index.html`);
 
         await page.tracing.start({
             path: filename
@@ -45,7 +49,7 @@ let processRawData = (filename, i) => {
 
         const inputHandle = await page.evaluateHandle(selector);
 
-        for (let j = 0; j<numberOfCreation; j++) {            
+        for (let j = 0; j < numberOfCreation; j++) {
             await inputHandle.type('New todo');
             await inputHandle.press('Enter');
         }
@@ -56,20 +60,20 @@ let processRawData = (filename, i) => {
 
         await browser.close();
     }
-    
+
     average = average / numberOftests;
 
     console.log(`\nAverage time for native : ${Math.ceil(average)} ms\n`);
 
-    /*average = 0;
+    average = 0;
 
     for (let i = 0; i < numberOftests; i++) {
-        browser = await puppeteer.launch({ headless: true });
+        browser = await puppeteer.launch({ headless: true, ignoreHTTPSErrors: true });
         page = await browser.newPage();
-        
+
         filename = `benchmarks-results/native-shadow-dom_lit-html/create-todos_${i}.json`;
 
-        await page.goto('http://localhost:8080/native-shadow-dom_lit-html/dist/index.html');
+        await page.goto(`${LOCALHOST}/native-shadow-dom_lit-html/dist/index.html`);
 
         await page.tracing.start({
             path: filename
@@ -77,7 +81,7 @@ let processRawData = (filename, i) => {
 
         const inputHandle = await page.evaluateHandle(selector);
 
-        for (let j = 0; j<numberOfCreation; j++) {            
+        for (let j = 0; j < numberOfCreation; j++) {
             await inputHandle.type('New todo');
             await inputHandle.press('Enter');
         }
@@ -88,28 +92,28 @@ let processRawData = (filename, i) => {
 
         await browser.close();
     }
-    
+
     average = average / numberOftests;
 
-    console.log(`\nAverage time for native + lit-html : ${Math.ceil(average)} ms\n`);*/
+    console.log(`\nAverage time for native + lit-html : ${Math.ceil(average)} ms\n`);
 
     average = 0;
 
     for (let i = 0; i < numberOftests; i++) {
-        browser = await puppeteer.launch({ headless: true });
+        browser = await puppeteer.launch({ headless: true, ignoreHTTPSErrors: true });
         page = await browser.newPage();
 
         filename = `benchmarks-results/polymer/create-todos_${i}.json`;
 
-        await page.goto('http://127.0.0.1:8080/polymer/build/es6-bundled/index.html');
+        await page.goto(`${LOCALHOST}/polymer/build/es6-bundled/index.html`);
 
         await page.tracing.start({
             path: filename
         });
-        
+
         const inputHandle = await page.evaluateHandle(selector);
 
-        for (let j = 0; j<numberOfCreation; j++) {            
+        for (let j = 0; j < numberOfCreation; j++) {
             await inputHandle.type('New todo');
             await inputHandle.press('Enter');
         }
@@ -128,20 +132,20 @@ let processRawData = (filename, i) => {
     average = 0;
 
     for (let i = 0; i < numberOftests; i++) {
-        browser = await puppeteer.launch({ headless: true });
+        browser = await puppeteer.launch({ headless: true, ignoreHTTPSErrors: true });
         page = await browser.newPage();
 
         filename = `benchmarks-results/polymer3/create-todos_${i}.json`;
 
-        await page.goto('http://127.0.0.1:8080/polymer3/build/es6-bundled/index.html');
+        await page.goto(`${LOCALHOST}/polymer3/build/es6-bundled/index.html`);
 
         await page.tracing.start({
             path: filename
         });
-        
+
         const inputHandle = await page.evaluateHandle(selector);
 
-        for (let j = 0; j<numberOfCreation; j++) {            
+        for (let j = 0; j < numberOfCreation; j++) {
             await inputHandle.type('New todo');
             await inputHandle.press('Enter');
         }
@@ -160,20 +164,20 @@ let processRawData = (filename, i) => {
     average = 0;
 
     for (let i = 0; i < numberOftests; i++) {
-        browser = await puppeteer.launch({ headless: true });
+        browser = await puppeteer.launch({ headless: true, ignoreHTTPSErrors: true });
         page = await browser.newPage();
 
         filename = `benchmarks-results/stencil/create-todos_${i}.json`;
 
-        await page.goto('http://127.0.0.1:8080/stencil/www/index.html');
+        await page.goto(`${LOCALHOST}/stencil/www/index.html`);
 
         await page.tracing.start({
             path: filename
         });
-        
+
         const inputHandle = await page.evaluateHandle(`document.querySelector('todo-input').querySelector('input')`);
 
-        for (let j = 0; j<numberOfCreation; j++) {            
+        for (let j = 0; j < numberOfCreation; j++) {
             await inputHandle.type('New todo');
             await inputHandle.press('Enter');
         }
@@ -187,17 +191,49 @@ let processRawData = (filename, i) => {
 
     average = average / numberOftests;
 
-    console.log(`\nAverage time for Stencil : ${Math.ceil(average)} ms\n`);
+    console.log(`\nAverage time for Stencil without PR : ${Math.ceil(average)} ms\n`);
+
+    average = 0;
+
+    for (let i = 0; i < numberOftests; i++) {
+        browser = await puppeteer.launch({ headless: true, ignoreHTTPSErrors: true });
+        page = await browser.newPage();
+
+        filename = `benchmarks-results/stencil-prerendered/create-todos_${i}.json`;
+
+        await page.goto(`${LOCALHOST}/stencil/www-prerendered/index.html`);
+
+        await page.tracing.start({
+            path: filename
+        });
+
+        const inputHandle = await page.evaluateHandle(`document.querySelector('todo-input').querySelector('input')`);
+
+        for (let j = 0; j < numberOfCreation; j++) {
+            await inputHandle.type('New todo');
+            await inputHandle.press('Enter');
+        }
+
+        await page.tracing.stop();
+
+        processRawData(filename, i);
+
+        await browser.close();
+    }
+
+    average = average / numberOftests;
+
+    console.log(`\nAverage time for Stencil with PR: ${Math.ceil(average)} ms\n`);
 
     /*average = 0;
 
     for (let i = 0; i < numberOftests; i++) {
-        browser = await puppeteer.launch({ headless: true })
+        browser = await puppeteer.launch({ headless: true, ignoreHTTPSErrors: true })
         page = await browser.newPage();
 
         filename = `benchmarks-results/angular-elements/create-todos_${i}.json`;
 
-        await page.goto('http://127.0.0.1:8080/angular-elements/dist/index.html');
+        await page.goto(`${LOCALHOST}/angular-elements/dist/index.html`);
 
         await page.tracing.start({
             path: filename
@@ -224,20 +260,20 @@ let processRawData = (filename, i) => {
     average = 0;
 
     for (let i = 0; i < numberOftests; i++) {
-        browser = await puppeteer.launch({ headless: true })
+        browser = await puppeteer.launch({ headless: true, ignoreHTTPSErrors: true });
         page = await browser.newPage();
 
         filename = `benchmarks-results/vue/create-todos_${i}.json`;
 
-        await page.goto('http://127.0.0.1:8080/vue.js/dist/index.html');
+        await page.goto(`${LOCALHOST}/vue.js/dist/index.html`);
 
         await page.tracing.start({
             path: filename
         });
-        
+
         const inputHandle = await page.evaluateHandle(selector);
 
-        for (let j = 0; j<numberOfCreation; j++) {            
+        for (let j = 0; j < numberOfCreation; j++) {
             await inputHandle.type('New todo');
             await inputHandle.press('Enter');
         }
@@ -252,5 +288,4 @@ let processRawData = (filename, i) => {
     average = average / numberOftests;
 
     console.log(`\nAverage time for Vue : ${Math.ceil(average)} ms\n`);
-    
 })();
