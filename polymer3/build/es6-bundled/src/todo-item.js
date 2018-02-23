82bcf16f5337eb37d70b7dc651213906494d57e1
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-02-23 15:19:32 +0100

    Cleaning polymer3 build

--- polymer3/build/es6-bundled/src/todo-item.js
+++
@@ -1,8 +0,0 @@
-import{Element as PolymerElement}from'../node_modules/@polymer/polymer/polymer-element.js';import'./todo-item-styles.js';export class TodoItem extends PolymerElement{static get is(){return'todo-item'}static get template(){return`
-        <style include="todo-item-style-element"></style>
-        <li class$="item [[isCompleted(checked)]]">
-            <input type="checkbox" value="{{checked}}" checked="{{checked::change}}">
-            <label>{{text}}</label>
-            <button class="destroy" on-click="handleOnRemove">x</button>
-        </li>
-      `}static get properties(){return{checked:{type:Boolean,value:!1},index:{type:Number},text:{type:String,value:''}}}handleOnRemove(){this.dispatchEvent(new CustomEvent('remove',{detail:this.index}))}handleOnChecked(){this.dispatchEvent(new CustomEvent('toggle',{detail:this.index}))}isCompleted(a){return a?'completed':''}}customElements.define(TodoItem.is,TodoItem);
--- polymer3/rollup.config.js
+++ polymer3/rollup.config.js
@@ -1,7 +1,7 @@
 export default {
     input: 'src/index.js',
     output: {
-        file: 'build/es6-bundled/src/index.tmp.js',
+        file: 'build/es6-bundled/src_tmp/index.js',
         format: 'es',
         sourcemap: false
     }
