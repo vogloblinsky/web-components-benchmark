d4173ad8d4bcf52e59a5ffb8b0815a72eac2e6cf
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-11-08 17:20:47 +0100

    feat(todo): add atomico fix #3

---
+++ todomvc/atomico/rollup.config.js
@@ -0,0 +1,50 @@
+import buble from "rollup-plugin-buble";
+import resolve from "rollup-plugin-node-resolve";
+import {
+    terser
+} from "rollup-plugin-terser";
+import commonjs from "rollup-plugin-commonjs";
+import filesize from "rollup-plugin-filesize";
+import pkg from "./package.json";
+import colors from "colors";
+import prepare from "rollup-prepare";
+import postcss from "rollup-plugin-postcss";
+import copy from 'rollup-plugin-copy';
+import cssnano from "cssnano";
+
+export default {
+    ...prepare({
+        pkg
+    }),
+    plugins: plugins(false)
+};
+
+/**
+ * Returns the generic plugins to be used for packaging
+ * @param {boolean} classes - lets you disable the transformation of classes
+ * @return {Array}
+ */
+function plugins(classes = true) {
+    return [
+        commonjs({
+            include: "node_modules/**"
+        }),
+        resolve(),
+        postcss({
+            plugins: [cssnano]
+        }),
+        buble({
+            jsx: "h",
+            transforms: {
+                classes
+            },
+            objectAssign: "Object.assign"
+        }),
+        terser(),
+        copy({
+            "node_modules/atomico/dist/atomico.umd.js": "public/atomico.umd.js",
+            verbose: true
+        }),
+        filesize()
+    ];
+}
