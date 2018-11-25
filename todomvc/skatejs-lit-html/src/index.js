78513096f4782359bd6022fe5b6becfcc4b1ca78
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-11-25 16:46:16 +0100

    feat(todo): update skatejs

--- todomvc/skatejs-lit-html/src/index.js
+++ todomvc/skatejs-lit-html/src/index.js
@@ -2,6 +2,6 @@ import App from "./app";
 import Input from "./input";
 import Item from "./item";
 
-customElements.define("todo-app", App);
+customElements.define("my-todo", App);
 customElements.define("todo-input", Input);
 customElements.define("todo-item", Item);
