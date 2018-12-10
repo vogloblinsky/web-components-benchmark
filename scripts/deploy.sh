dd0858ac974ba37887a936827c1b3a270993fe0a
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-12-10 16:59:57 +0100

    Travis file - deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,9 +9,7 @@ ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github
 
 echo "Compiling new static content"
 mkdir $TEMP_DIRECTORY || exit 1
-harp compile src $TEMP_DIRECTORY || exit 1
-cp .gitignore $TEMP_DIRECTORY || exit 1
-cp CNAME $TEMP_DIRECTORY || exit 1
+cp docs/index.html $TEMP_DIRECTORY || exit 1
 
 echo "Checking out gh-pages branch"
 git checkout -B gh-pages || exit 1
@@ -21,11 +19,10 @@ git rm -rf . || exit 1
 
 echo "Copying newly generated static content"
 cp -r $TEMP_DIRECTORY/* . || exit 1
-cp $TEMP_DIRECTORY/.gitignore . || exit 1
 
 echo "Pushing new content to $ORIGIN_URL"
 git config user.name "Travis-CI" || exit 1
-git config user.email "compodoc-github@gmail.com" || exit 1
+git config user.email "vincent.ogloblinsky@gmail.com" || exit 1
 
 git add -A . || exit 1
 git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1
