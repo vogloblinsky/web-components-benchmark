a7b700a099d206541b63a88e4c38da7b27466077
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-12-11 07:01:06 +0100

    fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,6 +10,7 @@ ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github
 echo "Compiling new static content"
 mkdir $TEMP_DIRECTORY || exit 1
 cp docs/index.html $TEMP_DIRECTORY || exit 1
+cp .gitignore $TEMP_DIRECTORY || exit 1
 
 echo "Checking out gh-pages branch"
 git checkout -B gh-pages || exit 1
@@ -17,8 +18,12 @@ git checkout -B gh-pages || exit 1
 echo "Removing old static content"
 git rm -rf . || exit 1
 
+echo "Static content"
+ls -al $TEMP_DIRECTORY || exit 1
+
 echo "Copying newly generated static content"
 cp -r $TEMP_DIRECTORY/* . || exit 1
+cp $TEMP_DIRECTORY/.gitignore . || exit 1
 
 echo "Pushing new content to $ORIGIN_URL"
 git config user.name "Travis-CI" || exit 1
