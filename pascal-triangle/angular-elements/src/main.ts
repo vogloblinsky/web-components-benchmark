388061c17b779e0edee5a8265de5eaa9aca61804
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-12-14 01:01:06 +0100

    fet(pascal): update angular-elements

--- pascal-triangle/angular-elements/src/main.ts
+++ pascal-triangle/angular-elements/src/main.ts
@@ -1,6 +1,8 @@
+import 'zone.js/dist/zone';
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { PascalTriangleModule } from './pascal-triangle.module';
+import { PascalTriangleModule } from './pascal-triangle/pascal-triangle.module';
 
 platformBrowserDynamic()
     .bootstrapModule(PascalTriangleModule)
--- pascal-triangle/angular-elements/src/pascal-triangle/pascal-triangle.module.ts
+++ pascal-triangle/angular-elements/src/pascal-triangle/pascal-triangle.module.ts
@@ -1,18 +1,21 @@
-import { NgModule, Injector, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 
 import { BrowserModule } from '@angular/platform-browser';
+
 import { PascalTriangleComponent } from './pascal-triangle.component';
+import { TriangleItemComponent } from '../triangle-item/triangle-item.component';
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [PascalTriangleComponent],
-    schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+    declarations: [PascalTriangleComponent, TriangleItemComponent],
     entryComponents: [PascalTriangleComponent]
 })
 export class PascalTriangleModule {
     constructor(private injector: Injector) {
-        const customElement = createCustomElement(PascalTriangleComponent, { injector });
+        const customElement = createCustomElement(PascalTriangleComponent, {
+            injector
+        });
         customElements.define('pascal-triangle', customElement);
     }
     ngDoBootstrap() {}
