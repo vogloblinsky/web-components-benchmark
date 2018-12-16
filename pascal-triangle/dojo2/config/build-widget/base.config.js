ba8375fdd42c1e77f47b82dacee4de80d6f17e72
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-12-16 03:36:16 +0100

    feat(apps): update dojo tooling

--- pascal-triangle/dojo2/config/build-widget/base.config.js
+++ pascal-triangle/dojo2/config/build-widget/base.config.js
@@ -1,11 +1,11 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const webpack = require("webpack");
-const path = require("path");
-const fs_1 = require("fs");
-const CssModulePlugin_1 = require("@dojo/webpack-contrib/css-module-plugin/CssModulePlugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const loaderUtils = require("loader-utils");
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const webpack = require('webpack');
+const path = require('path');
+const fs_1 = require('fs');
+const CssModulePlugin_1 = require('@dojo/webpack-contrib/css-module-plugin/CssModulePlugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const loaderUtils = require('loader-utils');
 const postcssPresetEnv = require('postcss-preset-env');
 const IgnorePlugin = require('webpack/lib/IgnorePlugin');
 const slash = require('slash');
@@ -14,7 +14,9 @@ const srcPath = path.join(basePath, 'src');
 const testPath = path.join(basePath, 'tests');
 const allPaths = [srcPath, testPath];
 const packageJsonPath = path.join(basePath, 'package.json');
-const packageJson = fs_1.existsSync(packageJsonPath) ? require(packageJsonPath) : {};
+const packageJson = fs_1.existsSync(packageJsonPath)
+    ? require(packageJsonPath)
+    : {};
 const packageName = packageJson.name || '';
 const tsLintPath = path.join(basePath, 'tslint.json');
 const tsLint = fs_1.existsSync(tsLintPath) ? require(tsLintPath) : false;
@@ -31,7 +33,10 @@ function getUMDCompatLoader(options) {
         loader: 'umd-compat-loader',
         options: {
             imports(module, context) {
-                const filePath = path.relative(basePath, path.join(context, module));
+                const filePath = path.relative(
+                    basePath,
+                    path.join(context, module)
+                );
                 let chunkName = slash(filePath);
                 Object.keys(bundles).some(name => {
                     if (bundles[name].indexOf(slash(filePath)) > -1) {
@@ -47,20 +52,30 @@ function getUMDCompatLoader(options) {
 }
 function getLocalIdent(loaderContext, localIdentName, localName, options) {
     if (!options.context) {
-        if (loaderContext.options && typeof loaderContext.options.context === 'string') {
+        if (
+            loaderContext.options &&
+            typeof loaderContext.options.context === 'string'
+        ) {
             options.context = loaderContext.options.context;
-        }
-        else {
+        } else {
             options.context = loaderContext.context;
         }
     }
-    const request = slash(path.relative(options.context, loaderContext.resourcePath));
+    const request = slash(
+        path.relative(options.context, loaderContext.resourcePath)
+    );
     options.content = `${options.hashPrefix}${request}+${localName}`;
     localIdentName = localIdentName.replace(/\[local\]/gi, localName);
-    const hash = loaderUtils.interpolateName(loaderContext, localIdentName, options);
-    return hash.replace(new RegExp('[^a-zA-Z0-9\\-_\u00A0-\uFFFF]', 'g'), '-').replace(/^((-?[0-9])|--)/, '_$1');
+    const hash = loaderUtils.interpolateName(
+        loaderContext,
+        localIdentName,
+        options
+    );
+    return hash
+        .replace(new RegExp('[^a-zA-Z0-9\\-_\u00A0-\uFFFF]', 'g'), '-')
+        .replace(/^((-?[0-9])|--)/, '_$1');
 }
-const removeEmpty = (items) => items.filter(item => item);
+const removeEmpty = items => items.filter(item => item);
 const banner = `
 [Dojo](https://dojo.io/)
 Copyright [JS Foundation](https://js.foundation/) & contributors
@@ -75,11 +90,17 @@ function colorToColorMod(style) {
 function webpackConfigFactory(args) {
     const elements = args.element ? [args.element] : args.elements;
     const jsonpIdent = args.element ? args.element.name : 'custom-elements';
-    const extensions = args.legacy ? ['.ts', '.tsx', '.js'] : ['.ts', '.tsx', '.mjs', '.js'];
-    const compilerOptions = args.legacy ? {} : { target: 'es6', module: 'esnext' };
+    const extensions = args.legacy
+        ? ['.ts', '.tsx', '.js']
+        : ['.ts', '.tsx', '.mjs', '.js'];
+    const compilerOptions = args.legacy
+        ? {}
+        : { target: 'es6', module: 'esnext' };
     const features = args.legacy ? args.features : ['chrome'];
     const postcssPresetConfig = {
-        browsers: args.legacy ? ['last 2 versions', 'ie >= 10'] : ['last 2 versions'],
+        browsers: args.legacy
+            ? ['last 2 versions', 'ie >= 10']
+            : ['last 2 versions'],
         insertBefore: {
             'color-mod-function': colorToColorMod
         },
@@ -94,7 +115,11 @@ function webpackConfigFactory(args) {
     const config = {
         entry: elements.reduce((entry, element) => {
             entry[element.name] = [
-                `imports-loader?widgetFactory=${element.path}!${path.join(__dirname, 'template', 'custom-element.js')}`
+                `imports-loader?widgetFactory=${element.path}!${path.join(
+                    __dirname,
+                    'template',
+                    'custom-element.js'
+                )}`
             ];
             return entry;
         }, {}),
@@ -102,7 +127,9 @@ function webpackConfigFactory(args) {
         output: {
             chunkFilename: `[name]-${packageJson.version}.js`,
             filename: `[name]-${packageJson.version}.js`,
-            jsonpFunction: getJsonpFunctionName(`-${packageName}-${jsonpIdent}`),
+            jsonpFunction: getJsonpFunctionName(
+                `-${packageName}-${jsonpIdent}`
+            ),
             libraryTarget: 'jsonp',
             path: path.resolve('./output')
         },
@@ -110,14 +137,15 @@ function webpackConfigFactory(args) {
             modules: [basePath, path.join(basePath, 'node_modules')],
             extensions
         },
-        devtool: 'source-map',
+        devtool: false,
         watchOptions: { ignored: /node_modules/ },
         plugins: removeEmpty([
             new CssModulePlugin_1.default(basePath),
             new webpack.BannerPlugin(banner),
             new IgnorePlugin(/request\/providers\/node/),
             new ExtractTextPlugin({
-                filename: (getPath) => getPath(`[name]-${packageJson.version}.css`)
+                filename: getPath =>
+                    getPath(`[name]-${packageJson.version}.css`)
             }),
             new webpack.NamedChunksPlugin(),
             new webpack.NamedModulesPlugin()
@@ -128,7 +156,11 @@ function webpackConfigFactory(args) {
                     test: /\.ts$/,
                     enforce: 'pre',
                     loader: 'tslint-loader',
-                    options: { configuration: tsLint, emitErrors: true, failOnHint: true }
+                    options: {
+                        configuration: tsLint,
+                        emitErrors: true,
+                        failOnHint: true
+                    }
                 },
                 {
                     test: /@dojo\/.*\.js$/,
@@ -146,7 +178,8 @@ function webpackConfigFactory(args) {
                     include: allPaths,
                     test: /.*\.m\.css?$/,
                     enforce: 'pre',
-                    loader: '@dojo/webpack-contrib/css-module-dts-loader?type=css'
+                    loader:
+                        '@dojo/webpack-contrib/css-module-dts-loader?type=css'
                 },
                 {
                     include: allPaths,
@@ -159,7 +192,12 @@ function webpackConfigFactory(args) {
                         getUMDCompatLoader({ bundles: args.bundles }),
                         {
                             loader: 'ts-loader',
-                            options: { onlyCompileBundledFiles: true, instance: jsonpIdent, transpileOnly: true, compilerOptions }
+                            options: {
+                                onlyCompileBundledFiles: true,
+                                instance: jsonpIdent,
+                                transpileOnly: true,
+                                compilerOptions
+                            }
                         }
                     ])
                 },
@@ -190,7 +228,8 @@ function webpackConfigFactory(args) {
                 },
                 {
                     test: /.*\.(gif|png|jpe?g|svg|eot|ttf|woff|woff2)$/i,
-                    loader: 'file-loader?hash=sha512&digest=hex&name=[hash:base64:8].[ext]'
+                    loader:
+                        'file-loader?hash=sha512&digest=hex&name=[hash:base64:8].[ext]'
                 },
                 {
                     test: /\.css$/,
@@ -218,7 +257,8 @@ function webpackConfigFactory(args) {
                                     modules: true,
                                     sourceMap: true,
                                     importLoaders: 1,
-                                    localIdentName: '[name]__[local]__[hash:base64:5]',
+                                    localIdentName:
+                                        '[name]__[local]__[hash:base64:5]',
                                     getLocalIdent
                                 }
                             },
@@ -226,7 +266,10 @@ function webpackConfigFactory(args) {
                                 loader: 'postcss-loader?sourceMap',
                                 options: {
                                     ident: 'postcss',
-                                    plugins: [require('postcss-import')(), postcssPresetEnv(postcssPresetConfig)]
+                                    plugins: [
+                                        require('postcss-import')(),
+                                        postcssPresetEnv(postcssPresetConfig)
+                                    ]
                                 }
                             }
                         ]
