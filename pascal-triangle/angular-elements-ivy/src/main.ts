eceb873cb374b4c355c29a3006b1dd8cce5220b7
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2019-08-24 01:09:06 +0200

    feat(pascal): bump ng ivy

--- pascal-triangle/angular-elements-ivy/src/main.ts
+++ pascal-triangle/angular-elements-ivy/src/main.ts
@@ -3,11 +3,9 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 
 import { PascalTriangleElement } from './app/pascal-triangle.element';
-import { TriangleItemElement } from './app/triangle-item.element';
 
 if (environment.production) {
     enableProdMode();
 }
 
-//customElements.define('triangle-item', TriangleItemElement);
 customElements.define('pascal-triangle', PascalTriangleElement);
